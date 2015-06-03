e1e35f96b7e0940a752f49722bc829b6e12c5d53
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-06-03 14:40:05 +0530

    Added sum function

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -11,10 +11,18 @@ describe Money do
 		expect(m.in_paisas).to eq(220)
 	end
 
+	it "having initialization inputs as 10Rs, 20p the equivalent value returned should be Rupees 10, Paise 20" do
+		m = Money.new(10,20)
+		expect(m.value).to eq("Rupees 10, Paise 20")
+	end
+
+
 	it "having values as 5Rs, 40p when added with another object having 8Rs, 30p the equivalent sum should be 13Rs, 70p" do
 		m1 = Money.new(5,40)
 		m2 = Money.new(8,30)
 		sum = m1 + m2
 		expect(sum.value).to eq("Rupees 13, Paise 70")
 	end
+
+
 end
